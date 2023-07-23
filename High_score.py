361dc05b24c1ca3f65c9203a8678de93ad32afbe
Pulkit Singh Kathayat <98012750+phantom339@users.noreply.github.com>
2023-07-23 09:36:06 +0530

    Add files via upload

---
+++ High_score.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+highest=0
+for scores in student_scores:
+    if highest < scores:
+        highest=scores
+print(f"The highest score in the class is: {highest}")
+
+
+
---
+++ adding_evens.py
@@ -0,0 +1,6 @@
+#Write your code below this row 👇
+a=0
+for num in range(1,101):
+    if num%2==0:
+        a+=num
+print(a)        
